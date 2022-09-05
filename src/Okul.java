acafaab338c5f15ba333e7cd7ae6da91b5c02cef
Emre Sulukan <esulukan34@gmail.com>
2022-09-05 23:25:31 +0300

    22.09.05

---
+++ src/Okul.java
@@ -0,0 +1,13 @@
+public class Okul {
+    //Okul bilgileri burada tutulacak ve degistirilemez olmalidir
+    //Okul bilgilerine proje icerisinden her class'dan ulasilabilmelidir
+    //Okul class'i SINGLETON PATTERN'e sahip olmalidir
+    static final String okulIsim = "Yildiz Kolaeji";
+    static final String adres = "Cankaya / Ankara";
+    static final String telefon = "0312 236 6554";
+
+    private Okul(){
+
+    }
+
+}
---
+++ src/Runner.java
@@ -0,0 +1,11 @@
+import java.io.FileNotFoundException;
+
+public class Runner {
+    public static void main(String[] args) throws InterruptedException, FileNotFoundException {
+        //Runner son derece sade olmali ve icinde sadece kullanacagimiz 3 method olacak
+        Depo.anaMenu();
+        // Ogretmen.ogretmenGetir();
+        // Ogrenci.feykOgrenciEkle();
+
+    }
+}
